b6a5148619d8c3268e393572fc2bbedb8e2e7654
Christopher <qaetre@gmail.com>
2023-05-20 14:39:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = "blahblah"
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!"
+const firstInitial = currentUser[0]
+const restOfName = currentUser.slice(1);
+const shortGreeting = "Welcome, " + firstInitial + "!"
